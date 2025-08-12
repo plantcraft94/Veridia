4339c3fd7f02e3c586571ea014c1c88165229f0b
Dam595 <96290770+Dam595@users.noreply.github.com>
2025-08-12 15:42:48 +0700

    aihealth

---
+++ Assets/Scripts/Enemy/AiHealth.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class AiHealth : MonoBehaviour
+{
+    [Header("Health Settings")]
+    public float maxHealth = 20f;
+    public float currentHealth;
+
+    [Header("Events")]
+    public UnityEvent onDeath;
+    public UnityEvent onDamage;
+    // qly animation, func
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        if (onDamage != null) onDamage.Invoke();
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        currentHealth += amount;
+        if (currentHealth > maxHealth) currentHealth = maxHealth;
+    }
+
+    public void Die()
+    {
+        if (onDeath != null) onDeath.Invoke();
+        Destroy(gameObject);
+    }
+}
